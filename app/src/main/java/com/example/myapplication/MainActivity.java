5f9bf6cabd32748cd84c689cdef8e7d04587e87e
NaminSaranga <47680610+NaminSaranga@users.noreply.github.com>
2019-08-17 15:10:09 +0530

    Update MainActivity.java
    change

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        system.out.println("i wrote that from github");
+        System.out.println("i wrote that from github");
     }
 }
